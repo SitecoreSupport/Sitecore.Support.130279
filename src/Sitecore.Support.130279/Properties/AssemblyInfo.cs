f0cfbe02d2f76ca30e191222cb4591cc3be5b4f4
DK\MVO <mvo@sitecore.net>
2017-04-07 15:37:12 +0300

    modify PageCode.cshtml and UsePhysicalViewsIfNewer config setting

--- src/Sitecore.Support.130279/Properties/AssemblyInfo.cs
+++ src/Sitecore.Support.130279/Properties/AssemblyInfo.cs
@@ -1,10 +1,9 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
-using Sitecore.Support;
 using WebActivatorEx;
 
 [assembly: AssemblyTitle("Sitecore.Support.130279")]
 [assembly: AssemblyProduct("Sitecore.Support.130279")]
 [assembly: ComVisible(false)]
-[assembly: PostApplicationStartMethod(typeof(RazorGeneratorMvcStart), "Start")]
+
 
